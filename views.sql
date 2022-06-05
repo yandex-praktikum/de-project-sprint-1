3b57d6b51cc6eedd068f88ecfffe3d773c651b19
nikart26 <76677964+nikart26@users.noreply.github.com>
2022-06-05 10:14:44 +0300

    Create views.sql

---
+++ views.sql
@@ -0,0 +1,14 @@
+create view analysis.orders as
+  select * from production.orders;
+
+create view analysis.orderitems as
+  select * from production.orderitems;
+  
+create view analysis.orderstatuses as
+  select * from production.orderstatuses;
+  
+create view analysis.products as
+  select * from production.products;
+  
+create view analysis.users as
+  select * from production.users;
